ed6e13bcb5635db03982fad9b4b2f005c630e9e2
IEKLIPSE-PC\iEklipse <steven.dien@outlook.com>
2023-07-10 14:16:19 -0400

    Crosshairs and HUDs

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -5,9 +5,26 @@
 #include "TimerManager.h"
 #include "Blueprint/UserWidget.h"
 
+
+void AShooterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HUD = CreateWidget(this, HUDClass);
+	if (HUD != nullptr)
+	{
+		HUD->AddToViewport();
+	}
+}
+
 void AShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus, bIsWinner);
+
+	if (HUD != nullptr)
+	{
+		HUD->RemoveFromViewport();
+	}
 
 	if (bIsWinner)
 	{
