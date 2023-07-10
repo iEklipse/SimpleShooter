bcfb22150301e6e8f92cc87ecd61cd14af6de660
IEKLIPSE-PC\iEklipse <steven.dien@outlook.com>
2023-07-09 20:04:52 -0400

    AI Aiming

--- Source/SimpleShooter/ShooterAIController.h
+++ Source/SimpleShooter/ShooterAIController.h
@@ -14,4 +14,8 @@ class SIMPLESHOOTER_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 };
